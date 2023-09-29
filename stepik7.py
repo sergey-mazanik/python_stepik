253bca44c48c1fd413fd8f861cddc14d870818c6
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-09-29 13:45:23 +0300

    add part of 7.11 unit

--- stepik7.py
+++ stepik7.py
@@ -794,4 +794,20 @@
 #     print(i[0])
 
 
+# a = {}
+# n = int(input())
+# for i in range(n):
+#     string = input()
+#     if string not in a:
+#         a[string] = 1
+#     else:
+#         a[string] += 1
+#
+# minimum = min(a.items(), key=lambda x: x[1])
+# maximum = max(a.items(), key=lambda x: x[1])
+# if len(a) >= 1:
+#     print(*maximum, sep=', ')
+#     print(*minimum, sep=', ')
+
+
 
