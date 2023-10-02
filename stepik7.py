af71693a872e46cabaa937f30b5d616b9afac6bb
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-10-02 18:05:29 +0300

    add 7.12 unit

--- stepik7.py
+++ stepik7.py
@@ -864,3 +864,28 @@
 #     a = input()
 # for k, v in sorted(comments.items(), key=lambda item: -len(item[1])):
 #     print(f'Количество уникальных комментаторов у {k} - {len(v)}')
+
+
+# def calculate(x: float, y: float, operation: str = 'a') -> None:
+#     def addition():
+#         print(x + y)
+#
+#     def subtraction():
+#         print(x - y)
+#
+#     def division():
+#         print('На ноль делить нельзя!' if y == 0 else x / y)
+#
+#     def multiplication():
+#         print(x * y)
+#
+#     if operation == 'a':
+#         addition()
+#     elif operation == 's':
+#         subtraction()
+#     elif operation == 'd':
+#         division()
+#     elif operation == 'm':
+#         multiplication()
+#     else:
+#         print('Ошибка. Данной операции не существует')
