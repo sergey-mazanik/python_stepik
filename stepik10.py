749c2567a00bf3b29eb09b1f32b40cc45d75aced
Sergey Mazanik <sergey.mazanik1@gmail.com>
2023-10-18 16:41:44 +0300

    add 10.5 unit

--- stepik10.py
+++ stepik10.py
@@ -424,4 +424,64 @@
 # print(*a, sep='\n')
 
 
+# 10.5 Функция zip
 
+# a = [5, 6, 7, 8]
+# b = [100, 200, 300, 40]
+# c = 'fndd'
+#
+# for i in range(4):
+#     print(a[i], b[i])
+# rez = zip(a, b, c)
+# print(list(rez))
+
+# rez = zip(a, b, c)
+# col1, col2, col3 = zip(*rez)
+# print(col1, col2, col3)
+
+
+# x = [1, 2, 3]
+# y = [4, 5, 6]
+# zipped = zip(x, y, strict=True)
+# result = list(zipped)
+# print(result)
+
+
+# keys = ['Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety', 'One hundred']
+# values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# print(dict(zip(keys, values)))
+
+
+# employees = [
+#     'Pankratiy', 'Innokentiy', 'Anfisa', 'Yaroslava', 'Veniamin',
+#     'Leonti', 'Daniil', 'Mishka', 'Lidochka',
+#     'Terenti', 'Vladik', 'Svetka', 'Maks', 'Yura', 'Sergei'
+# ]
+# identifiers = [77, 48, 88, 85, 76, 81, 62, 43, 5, 56, 17, 20, 37, 32, 96]
+#
+# employees_data = dict(zip(sorted(identifiers), sorted(employees)))
+# print(employees_data)
+
+
+# # Напишите определение функции zip_with_function
+# def zip_with_function(lst, func):
+#     return list(map(lambda args: func(*args), zip(*lst)))
+#
+#
+# def combine_strings(a: str, b: str) -> str:
+#     return a + b
+#
+#
+# def get_sum_two_numbers(a: int, b: int) -> int:
+#     return a + b
+#
+#
+# def get_sum_three_numbers(a: int, b: int, c: int) -> int:
+#     return a + b + c
+#
+#
+# assert zip_with_function([[1, 2, 4], [3, 5, 8]], get_sum_two_numbers) == [4, 7, 12]
+# assert zip_with_function([[10, 20], [30, 0]], get_sum_two_numbers) == [40, 20]
+# assert zip_with_function([[2, 5, 8], [3, 4, 7], [5, 6, 5]], get_sum_three_numbers) == [10, 15, 20]
+# assert zip_with_function([[1, 2, 3], [4, 5, 6], [7, 8, 9]], get_sum_three_numbers) == [12, 15, 18]
+# assert zip_with_function([["a", "b"], ["1", "2"]], combine_strings) == ['a1', 'b2']
